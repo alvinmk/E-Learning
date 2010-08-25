b2479354c5d7d0c30c3ea29c0766964f9906785d
Alvin Mattsson Kjellqvist <alvinmk@kth.se>
2010-08-25 09:26:33 +0000

    changePacket is not yet safe to run.

--- changePackage.pm
+++ changePackage.pm
@@ -10,6 +10,12 @@ sub edits() {
 	if($_ =~ m/.pm$/){
 		print "Filepath is $File::Find::name\n";
 		#Now when the file is found, change the package
+		$file = $_;
+		open(FILE,"+< $file");
+		$editFile = <FILE>; 
+		$file =~ s/Elearn::/Elearn1::/g;
+		print FILE $editFile;
+		close FILE;		
 	}
 	
 }
