ebbf638ab63c6e9b6e5777695859923c0f15349b
Alvin Mattsson Kjellqvist <alvinmk@kth.se>
2010-08-30 11:06:22 +0000

    ElearnDB

---
+++ lib/Elearn/Schema/E_learning/ElearnDB.pm
@@ -0,0 +1,16 @@
+package Elearn::Schema::E_learning::ElearnDB;
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Schema';
+
+__PACKAGE__->load_namespaces;
+
+
+# Created by DBIx::Class::Schema::Loader v0.04006 @ 2010-08-24 13:51:06
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:ELkvKWS8Vo1tJiU5+gCUhQ
+
+
+# You can replace this text with custom content, and it will be preserved on regeneration
+1;
