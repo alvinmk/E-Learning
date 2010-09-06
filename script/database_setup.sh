b37ece38031c733f682abd8a20740be5e7ef1dd9
Alvin <alvinmk+reg@gmail.com>
2010-09-06 16:45:37 +0200

    the database setup script created models and schemas in the wrong directorys, this is now fixed. This time for real

--- script/database_setup.sh
+++ script/database_setup.sh
@@ -18,7 +18,7 @@ if [ "$answer" = "y" ]
 		mysql -u elearn E_Learning < ../E_learningDB.mysql
 		#Update the perl scripts connecting the database
 		echo "Creating Elearnig database catalyst coupling"
-		script/elearn_create.pl model E_Learning::ElearnDB DBIC::Schema Elearn::Schema::E_Learing::ElearnDB create=static dbi:mysql:E_Learning 'elearn' '' '{ AutoCommit => 1 }'
+		script/elearn_create.pl model E_learning::ElearnDB DBIC::Schema Elearn::Schema::E_learning::ElearnDB create=static dbi:mysql:E_Learning 'elearn' '' '{ AutoCommit => 1 }'
 		chmod 777 Makefile.pl
 		perl ../Makefile.PL
 fi
